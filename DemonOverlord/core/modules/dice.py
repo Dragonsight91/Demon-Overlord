a398ee165647e9c7a170473503b3e1e6ed1e390a
Dragonsight91 <r.vanemelen@icloud.com>
2020-09-07 19:42:30 +0200

    finishing and cleaning up the dice module

--- DemonOverlord/core/modules/dice.py
+++ DemonOverlord/core/modules/dice.py
@@ -4,26 +4,36 @@ from DemonOverlord.core.util.responses import TextResponse, BadCommandResponse
 
 
 async def handler(command) -> discord.Embed:
-    if command.action == "d6" or command.action == "d8" or command.action == "d10" or command.action == "d12" or command.action == "d20":
+
+    # test if we have one of the defined dice classes
+    if (
+        command.action == "d6"
+        or command.action == "d8"
+        or command.action == "d10"
+        or command.action == "d12"
+        or command.action == "d20"
+    ):
         return DiceResponse(command)
+    
     else:
         BadCommandResponse(command)
-
-def get_dice(bot, die: str) -> str:
-    die = die[1:]
-    out = list()
-    for i in die:
-        out.append(bot.config.emoji["numbers"][int(i)])
-    return "".join(out)
 
 
 class DiceResponse(TextResponse):
     def __init__(self, command):
-        title = f"{get_dice(command.bot, command.action)} Dice - {command.action.upper()}"
+        # get the number emoji sequence 
+        def get_roll(bot, die: str) -> str:
+            out = list()
+            for i in die:
+                out.append(bot.config.emoji["numbers"][int(i)])
+            return "".join(out)
+
+        title = f":game_die: Dice - {command.action.upper()}"
         super().__init__(
             title,
             color=0x2CD5C9,
             icon=command.bot.config.izzymojis["what"] or "❓",
         )
-        result = get_dice(command.bot, f"d{randint(1, int(command.action[1:]))}")
-        self.description = f"The result is: {result}"
+        self.description = get_roll(
+            command.bot, str(randint(1, int(command.action[1:])))
+        )
